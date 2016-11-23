8eb7026ece14be308f837be27e766f692f616195
Carlos Sanchez <carlos.sanchez@nextthought.com>
2016-11-23 21:50:14 +0000

    move class to nti.app.contentlibrary
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.contentlibrary/trunk@101306 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ src/nti/app/contentlibrary/traversal.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+.. $Id$
+"""
+
+from __future__ import print_function, unicode_literals, absolute_import, division
+__docformat__ = "restructuredtext en"
+
+from zope import component
+from zope import interface
+
+from zope.location.interfaces import LocationError
+
+from zope.traversing.interfaces import ITraversable
+
+from pyramid.interfaces import IRequest
+
+from nti.contentlibrary.interfaces import IContentPackageLibrary
+
+@interface.implementer(ITraversable)
+@component.adapter(IContentPackageLibrary, IRequest)
+class LibraryTraversable(object):
+
+	def __init__(self, context, request):
+		self.context = context
+		self.request = request
+
+	def traverse(self, key, remaining_path):
+		try:
+			return self.context[key]
+		except KeyError:
+			raise LocationError(key)
