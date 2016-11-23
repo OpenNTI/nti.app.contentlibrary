1ac443546ea8753d082e46e9a1b9fd591af92bab
Carlos Sanchez <carlos.sanchez@nextthought.com>
2016-11-23 22:05:48 +0000

    move ntiid resolver to library app
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.contentlibrary/trunk@101308 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ src/nti/app/contentlibrary/ntiids.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+.. $Id$
+"""
+
+from __future__ import print_function, unicode_literals, absolute_import, division
+__docformat__ = "restructuredtext en"
+
+logger = __import__('logging').getLogger(__name__)
+
+from zope import component
+from zope import interface
+
+from nti.contentlibrary.interfaces import IContentPackageLibrary
+
+from nti.dataserver import authorization_acl as nacl
+
+from nti.dataserver.interfaces import ACLLocationProxy
+
+from nti.ntiids.interfaces import INTIIDResolver
+
+@interface.implementer(INTIIDResolver)
+class _ContentResolver(object):
+
+	def resolve(self, key):
+		result = None
+		library = component.queryUtility(IContentPackageLibrary)
+		path = library.pathToNTIID(key) if library else None
+		if path:
+			result = path[-1]
+			result = ACLLocationProxy(result, result.__parent__,
+									  result.__name__, nacl.ACL(result))
+		return result
