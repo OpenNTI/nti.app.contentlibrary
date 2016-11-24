ed3dee0f8e44e8d0c217cbbc2755728f347b6c21
Carlos Sanchez <carlos.sanchez@nextthought.com>
2016-11-24 03:59:54 +0000

    move library code out of the ugd_query view and register an utility
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.contentlibrary/trunk@101327 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ src/nti/app/contentlibrary/query.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+.. $Id$
+"""
+
+from __future__ import print_function, unicode_literals, absolute_import, division
+__docformat__ = "restructuredtext en"
+
+logger = __import__('logging').getLogger(__name__)
+
+from zope import component
+from zope import interface
+
+from nti.contentlibrary.indexed_data import get_catalog as lib_catalog
+
+from nti.contentlibrary.interfaces import IContentPackageLibrary
+
+from nti.contenttypes.presentation.interfaces import INTIAudio
+from nti.contenttypes.presentation.interfaces import INTIVideo
+from nti.contenttypes.presentation.interfaces import INTIPollRef
+from nti.contenttypes.presentation.interfaces import INTISurveyRef
+
+from nti.dataserver.interfaces import IIDContainersQuerier
+
+from nti.ntiids import ntiids
+
+from nti.site.site import get_component_hierarchy_names
+
+@interface.implementer(IIDContainersQuerier)
+class _NTIIDContainers(object):
+
+	def query(self, user, ntiid, include_stream, stream_only):
+		containers = ()
+		if ntiid == ntiids.ROOT:
+			containers = set(user.iterntiids(include_stream=include_stream,
+											 stream_only=stream_only))
+		else:
+			library = component.getUtility(IContentPackageLibrary)
+			containers = set(library.childrenOfNTIID(ntiid))
+			containers.add(ntiid)  # item
+
+			# include media containers.
+			catalog = lib_catalog()
+			if catalog is not None:  # test mode
+				# Should this be all types, or is that too expensive?
+				sites = get_component_hierarchy_names()
+				objects = catalog.search_objects(container_ntiids=containers,
+												 sites=sites,
+												 container_all_of=False,
+												 provided=(INTIVideo, INTIAudio,
+														   INTIPollRef, INTISurveyRef))
+				for obj in objects:
+					ntiid = getattr(obj, 'target', None) or obj.ntiid
+					containers.add(ntiid)
+
+		# We always include the unnamed root (which holds things like CIRCLED)
+		# NOTE: This is only in the stream. Normally we cannot store contained
+		# objects with an empty container key, so this takes internal magic
+		containers.add('')  # root
+		return containers
+	__call__ = query
