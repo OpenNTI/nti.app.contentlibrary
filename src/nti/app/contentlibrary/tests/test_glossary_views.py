190b02bc9336291bb2c339738878810156a4e8e2
Carlos Sanchez <carlos.sanchez@nextthought.com>
2016-11-24 17:30:20 +0000

    move glossary views to library pkg
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.contentlibrary/trunk@101339 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ src/nti/app/contentlibrary/tests/test_glossary_views.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function, unicode_literals, absolute_import, division
+__docformat__ = "restructuredtext en"
+
+# disable: accessing protected members, too many methods
+# pylint: disable=W0212,R0904
+
+from hamcrest import assert_that
+from hamcrest import contains_string
+
+import os.path
+
+import urllib
+
+from zope import component
+
+from nti.dictserver.storage import TrivialExcelCSVDataStorage
+
+from nti.app.testing.application_webtest import ApplicationLayerTest
+
+from nti.app.testing.decorators import WithSharedApplicationMockDS
+
+from nti.appserver.tests.test_application import TestApp
+
+from nti.dataserver.tests import mock_dataserver
+
+from nti.dictserver.tests import test_dictionary
+
+class TestApplicationGlossary(ApplicationLayerTest):
+
+	@WithSharedApplicationMockDS
+	def test_path_with_parens_no_container_no_verify(self):
+		# "We can hit the glossary of a new container. Does no real verification."
+		with mock_dataserver.mock_db_trans(self.ds):
+			self._create_user()
+
+		csv_dict = TrivialExcelCSVDataStorage(os.path.join(os.path.dirname(test_dictionary.__file__), 
+											  'nti_content_glossary.csv'))
+		component.provideUtility(csv_dict)
+
+		try:
+			testapp = TestApp(self.app)
+			path = '/dataserver2/users/sjohnson@nextthought.com/Pages(tag:NewcontainerResource)/Glossary/demo'
+			# path = urllib.quote( path )
+			res = testapp.get(path, extra_environ=self._make_extra_environ())
+
+			assert_that(res.body, contains_string(str('xml-stylesheet')))
+
+			path = '/dataserver2/users/sjohnson@nextthought.com/Pages(tag:NewcontainerResource)/Glossary/institutional theory'
+			path = urllib.quote(path)
+			res = testapp.get(path, extra_environ=self._make_extra_environ())
+
+			assert_that(res.body, contains_string(str('xml-stylesheet')))
+			assert_that(res.body, contains_string(str('institutional')))
+		finally:
+			component.getGlobalSiteManager().unregisterUtility(csv_dict)
