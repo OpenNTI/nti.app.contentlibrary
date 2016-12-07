55a3ff2ac8e5ec409b88ba5cbdf1233633dfcac4
Carlos Sanchez <carlos.sanchez@nextthought.com>
2016-12-07 22:08:18 +0000

    use container querier utility
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.contentlibrary/trunk@102192 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/contentlibrary/appserver/query.py
+++ src/nti/app/contentlibrary/appserver/query.py
@@ -13,8 +13,12 @@ from zope import component
 from zope import interface
 
 from nti.appserver.interfaces import IUserContainersQuerier
+from nti.appserver.interfaces import IContainedObjectsQuerier
 
 from nti.contentlibrary.indexed_data import get_catalog as lib_catalog
+
+from nti.contentlibrary.indexed_data.interfaces import IAudioIndexedDataContainer
+from nti.contentlibrary.indexed_data.interfaces import IVideoIndexedDataContainer
 
 from nti.contentlibrary.interfaces import IContentPackageLibrary
 
@@ -36,8 +40,10 @@ class _UserContainersQuerier(object):
 			containers = set(user.iterntiids(include_stream=include_stream,
 											 stream_only=stream_only))
 		else:
-			library = component.getUtility(IContentPackageLibrary)
-			containers = set(library.childrenOfNTIID(ntiid))
+			containers = set()
+			library = component.queryUtility(IContentPackageLibrary)
+			if library is not None:
+				containers.update(library.childrenOfNTIID(ntiid))
 			containers.add(ntiid)  # item
 
 			# include media containers.
@@ -59,4 +65,76 @@ class _UserContainersQuerier(object):
 		# objects with an empty container key, so this takes internal magic
 		containers.add('')  # root
 		return containers
+	__call__ = query
+
+@interface.implementer(IContainedObjectsQuerier)
+class _ContainedObjectsQuerier(object):
+
+	@classmethod
+	def _get_container_leaf(cls, ntiid):
+		library = component.getUtility(IContentPackageLibrary)
+		paths = library.pathToNTIID(ntiid) if library is not None else None
+		return paths[-1] if paths else None
+
+	@classmethod
+	def _container_assessments(cls, units, ntiid):
+		results = []
+		try:
+			from nti.assessment.interfaces import IQAssessmentItemContainer
+			for unit in units + [cls._get_container_leaf(ntiid)]:
+				container = IQAssessmentItemContainer(unit, None)
+				if not container:
+					continue
+				for asm_item in container.assessments():
+					results.append(asm_item)
+		except ImportError:
+			pass
+		return results
+
+	@classmethod
+	def _scan_media(cls, ntiid):
+		results = []
+		unit = cls._get_container_leaf(ntiid)
+		if unit is None:
+			return results
+		for provided in (IVideoIndexedDataContainer, IAudioIndexedDataContainer):
+			for media_data in provided(unit).values():
+				results.append(media_data)
+		return results
+
+	@classmethod
+	def _scan_quizzes(cls, ntiid):
+		library = component.queryUtility(IContentPackageLibrary)
+		# Quizzes are often subcontainers, so we look at the parent and its children
+		# TODO: Is this what we want for all implementations?
+		units = []
+		results = []
+		children = library.childrenOfNTIID(ntiid) if library is not None else ()
+		for unit in children or ():
+			unit = ntiids.find_object_with_ntiid(unit)
+			if unit is not None:
+				units.append(unit)
+		results.extend(cls._container_assessments(units, ntiid))
+		return results
+
+	@classmethod
+	def _get_legacy_contained(cls, container_ntiid):
+		results = []
+		results.extend(cls._scan_media(container_ntiid))
+		results.extend(cls._scan_quizzes(container_ntiid))
+		return results
+
+	@classmethod
+	def _get_contained(cls, container_ntiid):
+		catalog = lib_catalog()
+		sites = get_component_hierarchy_names()
+		objects = catalog.search_objects(container_ntiids=(container_ntiid,),
+										 sites=sites)
+		if not objects:
+			# Needed for non-persistent courses
+			objects = cls._get_legacy_contained(container_ntiid)
+		return objects
+
+	def query(self, ntiid):
+		return self._get_contained(ntiid)
 	__call__ = query
--- src/nti/app/contentlibrary/appserver/resolvers.py
+++ src/nti/app/contentlibrary/appserver/resolvers.py
@@ -12,17 +12,28 @@ logger = __import__('logging').getLogger(__name__)
 from zope import component
 from zope import interface
 
-from nti.appserver.interfaces import INTIIDRootResolver
+from nti.appserver.interfaces import IContainerLeafResolver
+from nti.appserver.interfaces import IContainerRootResolver
 
 from nti.contentlibrary.interfaces import IContentPackageLibrary
 
-@interface.implementer(INTIIDRootResolver)
-class _DefaultNTIIDRootResolver(object):
+class _DefaultContainerPathResolver(object):
+
+    index = None
 
     def __init__(self, *args):
         pass
 
-    def resolve(self, ntiid):
+    @classmethod
+    def resolve(cls, ntiid):
         library = component.queryUtility(IContentPackageLibrary)
         paths = library.pathToNTIID(ntiid) if library is not None else None
-        return paths[0] if paths else None
+        return paths[cls.index] if paths else None
+
+@interface.implementer(IContainerLeafResolver)
+class _DefaultContainerLeafResolver(_DefaultContainerPathResolver):
+    index = -1
+
+@interface.implementer(IContainerRootResolver)
+class _DefaultContainerRootResolver(_DefaultContainerPathResolver):
+    index = 0
