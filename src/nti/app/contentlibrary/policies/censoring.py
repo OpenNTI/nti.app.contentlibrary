298e2f7c0d6e0a31cb6cb64adb7ef7f522cfdf96
Carlos Sanchez <carlos.sanchez@nextthought.com>
2016-12-07 01:37:12 +0000

    Move unused censoring code
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.contentlibrary/trunk@102116 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ src/nti/app/contentlibrary/policies/censoring.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+.. $Id$
+"""
+
+from __future__ import print_function, unicode_literals, absolute_import, division
+__docformat__ = "restructuredtext en"
+
+logger = __import__('logging').getLogger(__name__)
+
+from zope import component
+from zope import interface
+
+from nti.contentfragments import censor
+
+from nti.contentfragments.interfaces import ICensoredContentPolicy
+
+from nti.contentlibrary.interfaces import IDelimitedHierarchyContentUnit
+
+from nti.dataserver.interfaces import IUser
+
+@interface.implementer(ICensoredContentPolicy)
+@component.adapter(IUser, IDelimitedHierarchyContentUnit)
+def user_filesystem_censor_policy(user, file_content_unit):
+	"""
+	Profanity filtering may be turned off in specific content units
+	by the use of a '.nti_disable_censoring' flag file.
+	"""
+	# TODO: maybe this could be handled with an ACL entry? The permission
+	# to post uncensored things?
+	if file_content_unit.does_sibling_entry_exist('.nti_disable_censoring'):
+		return None
+	return censor.DefaultCensoredContentPolicy()
