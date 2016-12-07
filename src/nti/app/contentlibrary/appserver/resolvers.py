f79a9e476fcd8185324e0dfe67c57bbc4ec977d4
Carlos Sanchez <carlos.sanchez@nextthought.com>
2016-12-07 01:19:59 +0000

    move appserver code to its own module
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.contentlibrary/trunk@102113 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ src/nti/app/contentlibrary/appserver/resolvers.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+.. $Id$
+"""
+
+from __future__ import print_function, unicode_literals, absolute_import, division
+__docformat__ = "restructuredtext en"
+
+logger = __import__('logging').getLogger(__name__)
+
+from zope import component
+from zope import interface
+
+from nti.appserver.interfaces import INTIIDRootResolver
+
+from nti.contentlibrary.interfaces import IContentPackageLibrary
+
+@interface.implementer(INTIIDRootResolver)
+class DefaultNTIIDRootResolver(object):
+
+    def __init__(self, *args):
+        pass
+
+    def resolve(self, ntiid):
+        library = component.queryUtility(IContentPackageLibrary)
+        paths = library.pathToNTIID(ntiid) if library is not None else None
+        return paths[0] if paths else None
