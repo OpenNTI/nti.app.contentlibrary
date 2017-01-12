90236cda6c2495cc89d1d81c3b806c1a1a2dea4c
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-01-12 18:57:27 +0000

    code reuse
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.contentlibrary/trunk@104186 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/contentlibrary/views/__init__.py
+++ src/nti/app/contentlibrary/views/__init__.py
@@ -15,15 +15,6 @@ from zope.container.contained import Contained
 
 from zope.traversing.interfaces import IPathAdapter
 
-from nti.contenttypes.presentation import PACKAGE_CONTAINER_INTERFACES
-
-
-def iface_of_thing(item):
-    for iface in PACKAGE_CONTAINER_INTERFACES:
-        if iface.providedBy(item):
-            return iface
-    return None
-
 
 @interface.implementer(IPathAdapter)
 class LibraryPathAdapter(Contained):
--- src/nti/app/contentlibrary/views/asset_views.py
+++ src/nti/app/contentlibrary/views/asset_views.py
@@ -33,8 +33,6 @@ from nti.app.contentlibrary.subscribers import update_indices_when_content_chang
 from nti.app.contentlibrary.utils import yield_content_packages
 from nti.app.contentlibrary.utils.common import remove_package_inaccessible_assets
 
-from nti.app.contentlibrary.views import iface_of_thing
-
 from nti.app.contentlibrary.views.sync_views import _AbstractSyncAllLibrariesView
 
 from nti.app.externalization.internalization import read_body_as_external_object
@@ -50,6 +48,8 @@ from nti.contentlibrary.indexed_data import get_library_catalog
 from nti.contenttypes.presentation import PACKAGE_CONTAINER_INTERFACES
 
 from nti.contenttypes.presentation.interfaces import IPresentationAssetContainer
+
+from nti.contenttypes.presentation import iface_of_asset as iface_of_thing
 
 from nti.dataserver import authorization as nauth
 
@@ -74,145 +74,153 @@ TOTAL = StandardExternalFields.TOTAL
 MIMETYPE = StandardExternalFields.MIMETYPE
 ITEM_COUNT = StandardExternalFields.ITEM_COUNT
 
+
 def _get_package_ntiids(values):
-	ntiids = values.get('ntiid') or values.get('ntiids')
-	if ntiids and isinstance(ntiids, six.string_types):
-		ntiids = ntiids.split()
-	return ntiids
+    ntiids = values.get('ntiid') or values.get('ntiids')
+    if ntiids and isinstance(ntiids, six.string_types):
+        ntiids = ntiids.split()
+    return ntiids
+
 
 def _read_input(request):
-	result = CaseInsensitiveDict()
-	if request:
-		if request.body:
-			values = read_body_as_external_object(request)
-		else:
-			values = request.params
-		result.update(values)
-	return result
+    result = CaseInsensitiveDict()
+    if request:
+        if request.body:
+            values = read_body_as_external_object(request)
+        else:
+            values = request.params
+        result.update(values)
+    return result
+
 
 @view_config(context=IDataserverFolder)
 @view_defaults(route_name='objects.generic.traversal',
-			   renderer='rest',
-			   request_method='GET',
-			   permission=nauth.ACT_NTI_ADMIN,
-			   name='GetPackagePresentationAssets')
+               renderer='rest',
+               request_method='GET',
+               permission=nauth.ACT_NTI_ADMIN,
+               name='GetPackagePresentationAssets')
 class GetPackagePresentationAssetsView(AbstractAuthenticatedView,
-									   ModeledContentUploadRequestUtilsMixin):
+                                       ModeledContentUploadRequestUtilsMixin):
 
-	def __call__(self):
-		params = CaseInsensitiveDict(self.request.params)
-		ntiids = _get_package_ntiids(params)
-		packages = list(yield_content_packages(ntiids=ntiids))
+    def __call__(self):
+        params = CaseInsensitiveDict(self.request.params)
+        ntiids = _get_package_ntiids(params)
+        packages = list(yield_content_packages(ntiids=ntiids))
 
-		catalog = get_library_catalog()
-		intids = component.getUtility(IIntIds)
+        catalog = get_library_catalog()
+        intids = component.getUtility(IIntIds)
 
-		total = 0
-		result = LocatedExternalDict()
-		result[ITEMS] = items = {}
-		sites = get_component_hierarchy_names()
-		for package in packages:
-			objects = catalog.search_objects(intids=intids,
-											 provided=PACKAGE_CONTAINER_INTERFACES,
-											 namespace=package.ntiid,
-											 sites=sites)
-			items[package.ntiid] = sorted(objects or (),
-										  key=lambda x: x.__class__.__name__)
-			total += len(items[package.ntiid])
+        total = 0
+        result = LocatedExternalDict()
+        result[ITEMS] = items = {}
+        sites = get_component_hierarchy_names()
+        for package in packages:
+            objects = catalog.search_objects(intids=intids,
+                                             provided=PACKAGE_CONTAINER_INTERFACES,
+                                             namespace=package.ntiid,
+                                             sites=sites)
+            items[package.ntiid] = sorted(objects or (),
+                                          key=lambda x: x.__class__.__name__)
+            total += len(items[package.ntiid])
 
-		result[ITEM_COUNT] = result[TOTAL] = total
-		return result
+        result[ITEM_COUNT] = result[TOTAL] = total
+        return result
+
 
 @view_config(context=IDataserverFolder)
 @view_defaults(route_name='objects.generic.traversal',
-			   renderer='rest',
-			   permission=nauth.ACT_NTI_ADMIN,
-			   name='ResetPackagePresentationAssets')
+               renderer='rest',
+               permission=nauth.ACT_NTI_ADMIN,
+               name='ResetPackagePresentationAssets')
 class ResetPackagePresentationAssetsView(_AbstractSyncAllLibrariesView):
 
-	def _unit_assets(self, package):
-		result = []
-		def recur(unit):
-			for child in unit.children or ():
-				recur(child)
-			container = IPresentationAssetContainer(unit)
-			for key, value in container.items():
-				provided = iface_of_thing(value)
-				if provided in PACKAGE_CONTAINER_INTERFACES:
-					result.append((key, value, container))
-		recur(package)
-		return result
+    def _unit_assets(self, package):
+        result = []
 
-	def _do_call(self):
-		total = 0
-		values = self.readInput()
-		ntiids = _get_package_ntiids(values)
-		force = is_true(values.get('force'))
+        def recur(unit):
+            for child in unit.children or ():
+                recur(child)
+            container = IPresentationAssetContainer(unit)
+            for key, value in container.items():
+                provided = iface_of_thing(value)
+                if provided in PACKAGE_CONTAINER_INTERFACES:
+                    result.append((key, value, container))
+        recur(package)
+        return result
 
-		result = LocatedExternalDict()
-		items = result[ITEMS] = {}
-		for package in yield_content_packages(ntiids):
-			seen = set()
-			removed = []
-			folder = find_interface(package, IHostPolicyFolder, strict=False)
-			with current_site(get_host_site(folder.__name__)):
-				registry = folder.getSiteManager()
-				# remove using catalog
-				removed.extend(clear_content_package_assets(package, force=force))
-				# remove anything left in containters
-				for ntiid, item, container in self._unit_assets(package):
-					if can_be_removed(item, force=force):
-						container.pop(ntiid, None)
-					if ntiid not in seen:
-						seen.add(ntiid)
-						provided = iface_of_thing(item)
-						if removed_registered(provided,
-										  	  ntiid,
-										  	  force=force,
-										   	  registry=registry) is not None:
-							removed.append(item)
-							remove_transaction_history(item)
-				# record output
-				items[package.ntiid] = removed
-				total += len(removed)
-		result[TOTAL] = total
-		return result
+    def _do_call(self):
+        total = 0
+        values = self.readInput()
+        ntiids = _get_package_ntiids(values)
+        force = is_true(values.get('force'))
+
+        result = LocatedExternalDict()
+        items = result[ITEMS] = {}
+        for package in yield_content_packages(ntiids):
+            seen = set()
+            removed = []
+            folder = find_interface(package, IHostPolicyFolder, strict=False)
+            with current_site(get_host_site(folder.__name__)):
+                registry = folder.getSiteManager()
+                # remove using catalog
+                removed.extend(clear_content_package_assets(package,
+														    force=force))
+                # remove anything left in containters
+                for ntiid, item, container in self._unit_assets(package):
+                    if can_be_removed(item, force=force):
+                        container.pop(ntiid, None)
+                    if ntiid not in seen:
+                        seen.add(ntiid)
+                        provided = iface_of_thing(item)
+                        if removed_registered(provided,
+                                              ntiid,
+                                              force=force,
+                                              registry=registry) is not None:
+                            removed.append(item)
+                            remove_transaction_history(item)
+                # record output
+                items[package.ntiid] = removed
+                total += len(removed)
+        result[TOTAL] = total
+        return result
+
 
 @view_config(context=IDataserverFolder)
 @view_defaults(route_name='objects.generic.traversal',
-			   renderer='rest',
-			   permission=nauth.ACT_NTI_ADMIN,
-			   name='SyncPackagePresentationAssets')
+               renderer='rest',
+               permission=nauth.ACT_NTI_ADMIN,
+               name='SyncPackagePresentationAssets')
 class SyncPackagePresentationAssetsView(_AbstractSyncAllLibrariesView):
 
-	def _do_call(self):
-		values = self.readInput()
-		result = LocatedExternalDict()
-		items = result[ITEMS] = []
-		ntiids = _get_package_ntiids(values)
-		for package in yield_content_packages(ntiids):
-			folder = find_interface(package, IHostPolicyFolder, strict=False)
-			with current_site(get_host_site(folder.__name__)):
-				items.append(package.ntiid)
-				update_indices_when_content_changes(package)
-		return result
+    def _do_call(self):
+        values = self.readInput()
+        result = LocatedExternalDict()
+        items = result[ITEMS] = []
+        ntiids = _get_package_ntiids(values)
+        for package in yield_content_packages(ntiids):
+            folder = find_interface(package, IHostPolicyFolder, strict=False)
+            with current_site(get_host_site(folder.__name__)):
+                items.append(package.ntiid)
+                update_indices_when_content_changes(package)
+        return result
+
 
 @view_config(context=IDataserverFolder)
 @view_defaults(route_name='objects.generic.traversal',
-			   renderer='rest',
-			   permission=nauth.ACT_NTI_ADMIN,
-			   name='RemovePackageInaccessibleAssets')
+               renderer='rest',
+               permission=nauth.ACT_NTI_ADMIN,
+               name='RemovePackageInaccessibleAssets')
 class RemovePackageInaccessibleAssetsView(AbstractAuthenticatedView,
-										  ModeledContentUploadRequestUtilsMixin):
+                                          ModeledContentUploadRequestUtilsMixin):
 
-	def readInput(self, value=None):
-		return _read_input(self.request)
+    def readInput(self, value=None):
+        return _read_input(self.request)
 
-	def __call__(self):
-		result = LocatedExternalDict()
-		endInteraction()
-		try:
-			result = remove_package_inaccessible_assets()
-		finally:
-			restoreInteraction()
-		return result
+    def __call__(self):
+        result = LocatedExternalDict()
+        endInteraction()
+        try:
+            result = remove_package_inaccessible_assets()
+        finally:
+            restoreInteraction()
+        return result
