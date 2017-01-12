530aebc9718d8bd6cc24da79bd2ae3a231e500f0
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-01-12 18:51:42 +0000

    add path adapter
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.contentlibrary/trunk@104185 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/contentlibrary/views/__init__.py
+++ src/nti/app/contentlibrary/views/__init__.py
@@ -9,10 +9,28 @@ __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
 
+from zope import interface
+
+from zope.container.contained import Contained
+
+from zope.traversing.interfaces import IPathAdapter
+
 from nti.contenttypes.presentation import PACKAGE_CONTAINER_INTERFACES
+
 
 def iface_of_thing(item):
     for iface in PACKAGE_CONTAINER_INTERFACES:
         if iface.providedBy(item):
             return iface
     return None
+
+
+@interface.implementer(IPathAdapter)
+class LibraryPathAdapter(Contained):
+
+    __name__ = 'Library'
+
+    def __init__(self, context, request):
+        self.context = context
+        self.request = request
+        self.__parent__ = context
