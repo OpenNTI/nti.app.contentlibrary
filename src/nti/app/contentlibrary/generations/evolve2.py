edd672c79e013d689a6d072df1156b66b159acb0
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-01-19 20:52:23 +0000

    stub out evolve
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.contentlibrary/trunk@104805 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/contentlibrary/generations/evolve2.py
+++ src/nti/app/contentlibrary/generations/evolve2.py
@@ -12,13 +12,34 @@ logger = __import__('logging').getLogger(__name__)
 generation = 2
 
 from zope import component
+from zope import interface
 
 from zope.component.hooks import site
 from zope.component.hooks import setHooks
 
-from zope.intid.interfaces import IIntIds
+from BTrees.OOBTree import OOBTree
 
-from nti.contentlibrary.indexed_data.catalog import install_library_catalog
+from nti.contentlibrary.interfaces import IContentPackageLibrary
+
+from nti.dataserver.interfaces import IDataserver
+from nti.dataserver.interfaces import IOIDResolver
+
+from nti.site.hostpolicy import get_all_host_sites
+
+
+@interface.implementer(IDataserver)
+class MockDataserver(object):
+
+    root = None
+
+    def get_by_oid(self, oid, ignore_creator=False):
+        resolver = component.queryUtility(IOIDResolver)
+        if resolver is None:
+            logger.warn("sing dataserver without a proper ISiteManager.")
+        else:
+            return resolver.get_object_by_oid(oid, ignore_creator=ignore_creator)
+        return None
+
 
 def do_evolve(context):
     setHooks()
@@ -26,15 +47,42 @@ def do_evolve(context):
     root = conn.root()
     ds_folder = root['nti.dataserver']
 
+    mock_ds = MockDataserver()
+    mock_ds.root = ds_folder
+    component.provideUtility(mock_ds, IDataserver)
+
     with site(ds_folder):
         assert  component.getSiteManager() == ds_folder.getSiteManager(), \
             "Hooks not installed?"
 
-        lsm = ds_folder.getSiteManager()
-        intids = lsm.getUtility(IIntIds)
+        for current_site in get_all_host_sites():
+            with site(current_site):
+                library = component.queryUtility(IContentPackageLibrary)
+                if library is None:
+                    continue
+                contentPackages = OOBTree()
+                contentUnitsByNTIID = OOBTree()
+                for package in library.contentPackages or ():
+                    contentPackages[package.ntiid] = package
 
-        install_library_catalog(ds_folder, intids)
+                def _recur(unit):
+                    contentUnitsByNTIID[unit.ntiid] = unit
+                    for child in unit.children:
+                        _recur(child)
+                for package in library.contentPackages or ():
+                    _recur(package)
+
+                library._contentPackages = contentPackages
+                library._contentUnitsByNTIID = contentUnitsByNTIID
+
+                for name in ('_content_packages_by_ntiid', '_content_units_by_ntiid'):
+                    try:
+                        delattr(library, name)
+                    except AttributeError:
+                        pass
+
         logger.info('Dataserver evolution %s done.', generation)
+    component.getGlobalSiteManager().unregisterUtility(mock_ds, IDataserver)
 
 
 def evolve(context):
