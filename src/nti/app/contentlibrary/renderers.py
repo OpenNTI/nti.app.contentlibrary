1c523cc5d8493518a1b7e9deb4c063a45f59616d
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-01-27 00:53:22 +0000

    pep8
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.contentlibrary/trunk@105278 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/contentlibrary/renderers.py
+++ src/nti/app/contentlibrary/renderers.py
@@ -16,24 +16,25 @@ from nti.app.contentlibrary.interfaces import IContentUnitInfo
 
 from nti.app.renderers.interfaces import IPreRenderResponseCacheController
 
+
 @component.adapter(IContentUnitInfo)
 @interface.implementer(IPreRenderResponseCacheController)
 class _ContentUnitInfoCacheController(object):
-	# rendering this doesn't take long, and we need the rendering
-	# process to decorate us with any sharing preferences that may change
-	# and update our modification stamp (this is why we can't be a
-	# subclass of _AbstractReliableLastModifiedCacheController)
+    # rendering this doesn't take long, and we need the rendering
+    # process to decorate us with any sharing preferences that may change
+    # and update our modification stamp (this is why we can't be a
+    # subclass of _AbstractReliableLastModifiedCacheController)
 
-	# We used to set a cache-contral max-age header of five minutes
-	# because that sped up navigation in the app noticebly. But it
-	# also led to testing problems if the tester switched accounts.
-	# The fix is to have the app cache these objects itself,
-	# and we go back to ETag/LastModified caching based on
-	# the rendered form.
-	# See Trell #2962 https://trello.com/c/1TGen4z1
+    # We used to set a cache-contral max-age header of five minutes
+    # because that sped up navigation in the app noticebly. But it
+    # also led to testing problems if the tester switched accounts.
+    # The fix is to have the app cache these objects itself,
+    # and we go back to ETag/LastModified caching based on
+    # the rendered form.
+    # See Trell #2962 https://trello.com/c/1TGen4z1
 
-	def __init__(self, context):
-		pass
+    def __init__(self, context):
+        pass
 
-	def __call__(self, context, system):
-		return system['request'].response
+    def __call__(self, context, system):
+        return system['request'].response
