54fb155fd91bad14d6136c9c135897cbceec61f0
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-01-27 00:06:28 +0000

    add mock ds
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.contentlibrary/trunk@105276 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/contentlibrary/generations/evolve4.py
+++ src/nti/app/contentlibrary/generations/evolve4.py
@@ -12,6 +12,7 @@ logger = __import__('logging').getLogger(__name__)
 generation = 4
 
 from zope import component
+from zope import interface
 
 from zope.component.hooks import site
 from zope.component.hooks import setHooks
@@ -20,21 +21,46 @@ from zope.intid.interfaces import IIntIds
 
 from nti.contentlibrary.indexed_data.catalog import install_assets_library_catalog
 
-def do_evolve(context):
+from nti.dataserver.interfaces import IDataserver
+from nti.dataserver.interfaces import IOIDResolver
+
+
+@interface.implementer(IDataserver)
+class MockDataserver(object):
+
+    root = None
+
+    def get_by_oid(self, oid, ignore_creator=False):
+        resolver = component.queryUtility(IOIDResolver)
+        if resolver is None:
+            logger.warn("sing dataserver without a proper ISiteManager.")
+        else:
+            return resolver.get_object_by_oid(oid, ignore_creator=ignore_creator)
+        return None
+
+
+def do_evolve(context, generation=generation):
     setHooks()
     conn = context.connection
     root = conn.root()
     ds_folder = root['nti.dataserver']
 
+    mock_ds = MockDataserver()
+    mock_ds.root = ds_folder
+    component.provideUtility(mock_ds, IDataserver)
+
     with site(ds_folder):
         assert  component.getSiteManager() == ds_folder.getSiteManager(), \
-            "Hooks not installed?"
+                "Hooks not installed?"
 
         lsm = ds_folder.getSiteManager()
         intids = lsm.getUtility(IIntIds)
 
         install_assets_library_catalog(ds_folder, intids)
         logger.info('Dataserver evolution %s done.', generation)
+
+    component.getGlobalSiteManager().unregisterUtility(mock_ds, IDataserver)
+    logger.info('Dataserver evolution %s done.', generation)
 
 
 def evolve(context):
