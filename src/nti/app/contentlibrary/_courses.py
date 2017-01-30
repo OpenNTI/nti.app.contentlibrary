4c3cf9f152d4f5a23ba42c655eaa4d43da5f196a
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-01-30 22:11:45 +0000

    add Associations subs
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.contentlibrary/trunk@105491 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/contentlibrary/_courses.py
+++ src/nti/app/contentlibrary/_courses.py
@@ -10,6 +10,7 @@ __docformat__ = "restructuredtext en"
 logger = __import__('logging').getLogger(__name__)
 
 from zope import component
+from zope import interface
 
 from zope.lifecycleevent.interfaces import IObjectAddedEvent
 from zope.lifecycleevent.interfaces import IObjectRemovedEvent
@@ -19,7 +20,15 @@ from zope.security.interfaces import IPrincipal
 from nti.app.contentlibrary._permissioned import _set_user_ticket
 from nti.app.contentlibrary._permissioned import _memcached_client
 
+from nti.contentlibrary.interfaces import IContentUnit
+from nti.contentlibrary.interfaces import IContentPackage
+from nti.contentlibrary.interfaces import IContentUnitAssociations
+
 from nti.contenttypes.courses.interfaces import ICourseInstanceEnrollmentRecord
+
+from nti.contenttypes.courses.utils import get_courses_for_packages
+
+from nti.traversal.traversal import find_interface
 
 
 def _on_operation_on_scope_membership(record, event):
@@ -37,3 +46,17 @@ def _on_enroll_record(record, event):
 @component.adapter(ICourseInstanceEnrollmentRecord, IObjectRemovedEvent)
 def _on_unenroll_record(record, event):
     _on_operation_on_scope_membership(record, event)
+
+
+@component.adapter(IContentUnit)
+@interface.implementer(IContentUnitAssociations)
+class _CourseContentUnitAssociations(object):
+
+    def __init__(self, *args):
+        pass
+
+    def associations(self, context):
+        package = find_interface(context, IContentPackage, strict=False)
+        if package is not None:
+            return get_courses_for_packages(packages=(package.ntiid,))
+        return ()
