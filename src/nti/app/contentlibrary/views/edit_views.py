445267a9516fa9e56674dea220429c05ac132bd2
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-03-02 22:55:49 +0000

    use str
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.contentlibrary/trunk@107863 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/contentlibrary/views/edit_views.py
+++ src/nti/app/contentlibrary/views/edit_views.py
@@ -50,8 +50,6 @@ from nti.app.contentlibrary.views import LibraryPathAdapter
 
 from nti.appserver.ugd_edit_views import UGDPutView
 
-from nti.base._compat import bytes_
-
 from nti.common.string import is_true
 
 from nti.contentlibrary.interfaces import IContentValidator
@@ -177,8 +175,8 @@ class ContentPackageMixin(object):
                 content = source.read()
                 contentType = source.contentType or RST_MIMETYPE
         if content:
-            content = bytes_(content)
-            contentType = bytes_(contentType or RST_MIMETYPE)
+            content = str(content)
+            contentType = str(contentType or RST_MIMETYPE)
             self._validate(content, contentType)
         return content, contentType
 
@@ -337,7 +335,7 @@ class ContentPackageContentsGetView(AbstractAuthenticatedView,
     def as_attachment(self):
         response = self.request.response
         contents = self._get_contents()
-        contentType = bytes_(self.context.contentType or RST_MIMETYPE)
+        contentType = str(self.context.contentType or RST_MIMETYPE)
         ext = mimetypes.guess_extension(contentType) or ".rst"
         downloadName = "contents%s" % ext
         headers = getHeaders(self.context,
@@ -354,7 +352,7 @@ class ContentPackageContentsGetView(AbstractAuthenticatedView,
         result = ContentUnitContents()
         result.ntiid = self.context.ntiid
         result.contents = self._get_contents()
-        result.contentType = bytes_(self.context.contentType or RST_MIMETYPE)
+        result.contentType = str(self.context.contentType or RST_MIMETYPE)
         return result
 
     def __call__(self):
