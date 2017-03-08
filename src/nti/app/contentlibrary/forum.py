821642fb5a81ef5620fa7c65f29b3b6ea75e87a3
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-03-08 20:45:56 +0000

    pep8
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.contentlibrary/trunk@108282 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/contentlibrary/forum.py
+++ src/nti/app/contentlibrary/forum.py
@@ -43,43 +43,45 @@ from nti.site.site import get_component_hierarchy_names
 
 from nti.traversal.traversal import find_interface
 
+
 @interface.implementer(IContentBoard)
 class ContentBoard(GeneralBoard):
-	mime_type = mimeType = 'application/vnd.nextthought.forums.contentboard'
+    mime_type = mimeType = 'application/vnd.nextthought.forums.contentboard'
 
-	# Override things related to ntiids.
-	# These don't have global names, so they must be referenced
-	# by OID. We are also IUseOIDForNTIID so our children
-	# inherit this.
-	NTIID_TYPE = _ntiid_type = TYPE_OID
-	NTIID = cachedIn('_v_ntiid')(to_external_ntiid_oid)
+    # Override things related to ntiids.
+    # These don't have global names, so they must be referenced
+    # by OID. We are also IUseOIDForNTIID so our children
+    # inherit this.
+    NTIID_TYPE = _ntiid_type = TYPE_OID
+    NTIID = cachedIn('_v_ntiid')(to_external_ntiid_oid)
 
-	# Who owns this? Who created it?
-	# Right now, we're saying "system" did it...
-	# see also the sharing targets
-	creator = system_user
+    # Who owns this? Who created it?
+    # Right now, we're saying "system" did it...
+    # see also the sharing targets
+    creator = system_user
 
-	def createDefaultForum(self):
-		if ContentForum.__default_name__ in self:
-			return self[ContentForum.__default_name__]
+    def createDefaultForum(self):
+        if ContentForum.__default_name__ in self:
+            return self[ContentForum.__default_name__]
 
-		forum = ContentForum()
-		forum.creator = self.creator
-		self[forum.__default_name__] = forum
-		forum.title = _('Forum')
+        forum = ContentForum()
+        forum.creator = self.creator
+        self[forum.__default_name__] = forum
+        forum.title = _('Forum')
 
-		errors = schema.getValidationErrors(IContentForum, forum)
-		if errors:
-			__traceback_info__ = errors
-			raise errors[0][1]
-		return forum
+        errors = schema.getValidationErrors(IContentForum, forum)
+        if errors:
+            __traceback_info__ = errors
+            raise errors[0][1]
+        return forum
+
 
 @interface.implementer(IContentBoard)
 def ContentBoardAdapter(context):
-	board = AnnotatableBoardAdapter(context, ContentBoard, IContentBoard)
-	if board.creator is None or IContentPackageBundle.providedBy(context):
-		board.creator = system_user
-	return board
+    board = AnnotatableBoardAdapter(context, ContentBoard, IContentBoard)
+    if board.creator is None or IContentPackageBundle.providedBy(context):
+        board.creator = system_user
+    return board
 
 # Forum
 
@@ -87,19 +89,22 @@ from nti.app.contentlibrary.interfaces import IContentForum
 
 from nti.dataserver.contenttypes.forums.forum import GeneralForum
 
+
 @interface.implementer(IContentForum)
 class ContentForum(GeneralForum):
-	__external_can_create__ = True
-	mime_type = mimeType = 'application/vnd.nextthought.forums.contentforum'
 
-	@property
-	def _ntiid_mask_creator(self):
-		return (self.creator != system_user)
+    __external_can_create__ = True
 
-	def xxx_isReadableByAnyIdOfUser(self, remote_user, my_ids, family):
-		# if we get here, we're authenticated
-		# See above about the sharing stuff
-		return True
+    mime_type = mimeType = 'application/vnd.nextthought.forums.contentforum'
+
+    @property
+    def _ntiid_mask_creator(self):
+        return (self.creator != system_user)
+
+    def xxx_isReadableByAnyIdOfUser(self, remote_user, my_ids, family):
+        # if we get here, we're authenticated
+        # See above about the sharing stuff
+        return True
 
 # Topic
 
@@ -111,65 +116,67 @@ from nti.dataserver.contenttypes.forums.topic import GeneralHeadlineTopic
 
 from nti.dataserver.interfaces import IDefaultPublished
 
+
 @interface.implementer(IContentHeadlineTopic)
 class ContentHeadlineTopic(GeneralHeadlineTopic):
 
-	__external_can_create__ = True
+    __external_can_create__ = True
 
-	mimeType = 'application/vnd.nextthought.forums.contentheadlinetopic'
+    mimeType = 'application/vnd.nextthought.forums.contentheadlinetopic'
 
-	DEFAULT_SHARING_TARGETS = ('Everyone',)
-	publicationSharingTargets = DEFAULT_SHARING_TARGETS
+    DEFAULT_SHARING_TARGETS = ('Everyone',)
+    publicationSharingTargets = DEFAULT_SHARING_TARGETS
 
-	@property
-	def sharingTargetsWhenPublished(self):
-		# Instead of returning the default set from super, which would return
-		# the dynamic memberships of the *creator* of this object, we
-		# make it visible to the site community or the world
-		# XXX NOTE: This will change as I continue to flesh out
-		# the permissioning of the content bundles themselves
-		# auth = IPrincipal( AUTHENTICATED_GROUP_NAME )
-		# interface.alsoProvides(auth, IEntity)
-		result = []
-		for name in self.publicationSharingTargets:
-			entity = users.Entity.get_entity(name)
-			if entity is not None:
-				result.append(entity)
-		return tuple(result)
+    @property
+    def sharingTargetsWhenPublished(self):
+        # Instead of returning the default set from super, which would return
+        # the dynamic memberships of the *creator* of this object, we
+        # make it visible to the site community or the world
+        # XXX NOTE: This will change as I continue to flesh out
+        # the permissioning of the content bundles themselves
+        # auth = IPrincipal( AUTHENTICATED_GROUP_NAME )
+        # interface.alsoProvides(auth, IEntity)
+        result = []
+        for name in self.publicationSharingTargets:
+            entity = users.Entity.get_entity(name)
+            if entity is not None:
+                result.append(entity)
+        return tuple(result)
 
-	@property
-	def flattenedSharingTargetNames(self):
-		result = super(ContentHeadlineTopic, self).flattenedSharingTargetNames
-		if 'Everyone' in result:
-			result.add('system.Everyone')
-		return result
+    @property
+    def flattenedSharingTargetNames(self):
+        result = super(ContentHeadlineTopic, self).flattenedSharingTargetNames
+        if 'Everyone' in result:
+            result.add('system.Everyone')
+        return result
 
-	def isSharedWith(self, wants):
-		res = super(ContentHeadlineTopic, self).isSharedWith(wants)
-		if not res:
-			# again, implicitly with everyone
-			res = IDefaultPublished.providedBy(self)
-		return res
+    def isSharedWith(self, wants):
+        res = super(ContentHeadlineTopic, self).isSharedWith(wants)
+        if not res:
+            # again, implicitly with everyone
+            res = IDefaultPublished.providedBy(self)
+        return res
 
-	def publish(self):
-		folder = find_interface(self, IHostPolicyFolder, strict=False)
-		if folder is not None:
-			# find a community in site hierarchy
-			names = chain((folder.__name__,), get_component_hierarchy_names())
-			for name in names:
-				comm = users.Entity.get_entity(name or u'')
-				if ICommunity.providedBy(comm): # we have community
-					self.publicationSharingTargets = (name,)
-					break
-			else:
-				self.publicationSharingTargets = () # no community
-		else: # global
-			self.publicationSharingTargets = self.DEFAULT_SHARING_TARGETS
-		return super(ContentHeadlineTopic, self).publish()
+    def publish(self):
+        folder = find_interface(self, IHostPolicyFolder, strict=False)
+        if folder is not None:
+            # find a community in site hierarchy
+            names = chain((folder.__name__,), get_component_hierarchy_names())
+            for name in names:
+                comm = users.Entity.get_entity(name or u'')
+                if ICommunity.providedBy(comm):  # we have community
+                    self.publicationSharingTargets = (name,)
+                    break
+            else:
+                self.publicationSharingTargets = ()  # no community
+        else:  # global
+            self.publicationSharingTargets = self.DEFAULT_SHARING_TARGETS
+        return super(ContentHeadlineTopic, self).publish()
 
-	def unpublish(self):
-		self.publicationSharingTargets = self.DEFAULT_SHARING_TARGETS  # restore
-		return super(ContentHeadlineTopic, self).unpublish()
+    def unpublish(self):
+        # restore
+        self.publicationSharingTargets = self.DEFAULT_SHARING_TARGETS
+        return super(ContentHeadlineTopic, self).unpublish()
 
 # Posts
 
@@ -179,18 +186,22 @@ from nti.app.contentlibrary.interfaces import IContentHeadlinePost
 from nti.dataserver.contenttypes.forums.post import GeneralHeadlinePost
 from nti.dataserver.contenttypes.forums.post import GeneralForumComment
 
+
 @interface.implementer(IContentHeadlinePost)
 class ContentHeadlinePost(GeneralHeadlinePost):
-	mime_type = mimeType = 'application/vnd.nextthought.forums.contentheadlinepost'
+
+    mime_type = mimeType = 'application/vnd.nextthought.forums.contentheadlinepost'
+
 
 @interface.implementer(IContentCommentPost)
 class ContentCommentPost(GeneralForumComment):
-	mime_type = mimeType = 'application/vnd.nextthought.forums.contentforumcomment'
 
-	def xxx_isReadableByAnyIdOfUser(self, remote_user, my_ids, family):
-		# if we get here, we're authenticated
-		# See above about the sharing stuff
-		return True
+    mime_type = mimeType = 'application/vnd.nextthought.forums.contentforumcomment'
+
+    def xxx_isReadableByAnyIdOfUser(self, remote_user, my_ids, family):
+        # if we get here, we're authenticated
+        # See above about the sharing stuff
+        return True
 
 # ACLs
 
@@ -203,28 +214,29 @@ from nti.dataserver.contenttypes.forums.acl import CommunityBoardACLProvider
 
 from nti.dataserver.interfaces import AUTHENTICATED_GROUP_NAME
 
+
 @component.adapter(IContentBoard)
 class _ContentBoardACLProvider(CommunityBoardACLProvider):
-	"""
-	We want exactly the same thing as the community gets:
-	admins can create/delete forums, and the creator gets nothing special,
-	with nothing inherited.
-	"""
+    """
+    We want exactly the same thing as the community gets:
+    admins can create/delete forums, and the creator gets nothing special,
+    with nothing inherited.
+    """
 
-	def _extend_acl_after_creator_and_sharing(self, acl):
-		acl.append(ace_allowing(AUTHENTICATED_GROUP_NAME, ACT_READ, ContentBoard))
+    def _extend_acl_after_creator_and_sharing(self, acl):
+        acl.append(ace_allowing(AUTHENTICATED_GROUP_NAME, ACT_READ, ContentBoard))
+        # acl.append( ace_allowing( prin, ACT_CREATE, ContentBoard ))
+        super(_ContentBoardACLProvider, self)._extend_acl_after_creator_and_sharing(acl)
 
-		# acl.append( ace_allowing( prin, ACT_CREATE, ContentBoard ))
-		super(_ContentBoardACLProvider, self)._extend_acl_after_creator_and_sharing(acl)
 
 @component.adapter(IContentForum)
 class _ContentForumACLProvider(CommunityForumACLProvider):
-	"""
-	Lets everyone create entries inside it right now.
-	"""
+    """
+    Lets everyone create entries inside it right now.
+    """
 
-	def _get_sharing_target_names(self):
-		return ('Everyone', AUTHENTICATED_GROUP_NAME)
+    def _get_sharing_target_names(self):
+        return ('Everyone', AUTHENTICATED_GROUP_NAME)
 
 # Forum decorators
 
@@ -235,22 +247,26 @@ from nti.externalization.singleton import SingletonDecorator
 
 from nti.links.links import Link
 
-# These imports are broken out explicitly for speed (avoid runtime attribute lookup)
+# These imports are broken out explicitly for speed (avoid runtime
+# attribute lookup)
 LINKS = StandardExternalFields.LINKS
+
 
 @interface.implementer(IExternalMappingDecorator)
 class ContentBoardLinkDecorator(object):
-	# XXX Very similar to the decorators for Community and PersonalBlog;
-	# can we unify these?
-	__metaclass__ = SingletonDecorator
+    # XXX Very similar to the decorators for Community and PersonalBlog;
+    # can we unify these?
+    __metaclass__ = SingletonDecorator
 
-	def decorateExternalMapping(self, context, mapping):
-		# TODO: This may be slow, if the forum doesn't persistently
-		# exist and we keep creating it and throwing it away (due to
-		# not commiting on GET)
-		board = IContentBoard(context, None)
-		if board is not None:  # Not checking security. If the community is visible to you, the forum is too
-			the_links = mapping.setdefault(LINKS, [])
-			link = Link(board, rel=board.__name__)
-			# link_belongs_to_user( link, context )
-			the_links.append(link)
+    def decorateExternalMapping(self, context, mapping):
+        # TODO: This may be slow, if the forum doesn't persistently
+        # exist and we keep creating it and throwing it away (due to
+        # not commiting on GET)
+        board = IContentBoard(context, None)
+        # Not checking security. If the community is visible to you, the forum
+        # is too
+        if board is not None:
+            the_links = mapping.setdefault(LINKS, [])
+            link = Link(board, rel=board.__name__)
+            # link_belongs_to_user( link, context )
+            the_links.append(link)
