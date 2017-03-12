c0b1115570121b767e6e5c2a630cb54c5e08e1c1
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-03-12 04:44:19 +0000

    more admin views
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.contentlibrary/trunk@108673 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/contentlibrary/views/admin_views.py
+++ src/nti/app/contentlibrary/views/admin_views.py
@@ -24,6 +24,8 @@ from nti.app.base.abstract_views import AbstractAuthenticatedView
 
 from nti.app.contentlibrary import MessageFactory as _
 
+from nti.app.contentlibrary.views import LibraryPathAdapter
+
 from nti.app.externalization.error import raise_json_error
 
 from nti.common.string import is_true
@@ -32,18 +34,27 @@ from nti.contentlibrary.interfaces import IContentPackage
 from nti.contentlibrary.interfaces import IContentPackageLibrary
 from nti.contentlibrary.interfaces import resolve_content_unit_associations
 
+from nti.contentlibrary import ALL_CONTENT_MIMETYPES
+
+from nti.contentlibrary.utils import get_content_packages
+
 from nti.dataserver import authorization as nauth
 
 from nti.externalization.externalization import to_external_object
 from nti.externalization.externalization import StandardExternalFields
+
+from nti.externalization.interfaces import LocatedExternalDict
 
 from nti.links.links import Link
 
 from nti.property.property import Lazy
 
 CLASS = StandardExternalFields.CLASS
+ITEMS = StandardExternalFields.ITEMS
 LINKS = StandardExternalFields.LINKS
+TOTAL = StandardExternalFields.TOTAL
 MIMETYPE = StandardExternalFields.MIMETYPE
+ITEM_COUNT = StandardExternalFields.ITEM_COUNT
 
 
 @view_config(context=IContentPackage)
@@ -109,4 +120,35 @@ class ContentPackageDeleteView(AbstractAuthenticatedView):
                                        associations)
         result = hexc.HTTPNoContent()
         result.last_modified = time.time()
+        return result
+
+
+@view_config(context=LibraryPathAdapter)
+@view_defaults(route_name='objects.generic.traversal',
+               renderer='rest',
+               request_method='POST',
+               permission=nauth.ACT_NTI_ADMIN)
+class RemoveInvalidPackagesView(AbstractAuthenticatedView):
+
+    @Lazy
+    def _library(self):
+        library = component.queryUtility(IContentPackageLibrary)
+        return library
+
+    def _do_delete_object(self, theObject, event=True):
+        library = self._library
+        library.remove(theObject, event=event)
+        return theObject
+
+    def __call__(self):
+        library = self._library
+        result = LocatedExternalDict()
+        result[ITEMS] = items = {}
+        packages = get_content_packages(mime_types=ALL_CONTENT_MIMETYPES)
+        for package in packages:
+            stored = library.get(package.ntiid)
+            if stored is None:
+                self._do_delete_object(package)
+                items[package.ntiid] = package.ntiid
+        result[TOTAL] = result[ITEM_COUNT] = len(items)
         return result
