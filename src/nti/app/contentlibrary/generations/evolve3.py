0f9797bbda6cf8127fa754c59ca12551cbc3ae6c
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-03-16 14:56:05 +0000

    disable 4 prod release
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.contentlibrary/trunk@108992 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/contentlibrary/generations/evolve3.py
+++ src/nti/app/contentlibrary/generations/evolve3.py
@@ -12,35 +12,10 @@ logger = __import__('logging').getLogger(__name__)
 generation = 3
 
 from zope import component
-from zope import interface
 
 from zope.component.hooks import site
-from zope.component.hooks import setHooks
-
-from zope.intid.interfaces import IIntIds
-
-from nti.contentlibrary.index import install_library_catalog
 
 from nti.contentlibrary.interfaces import IContentPackageLibrary
-
-from nti.dataserver.interfaces import IDataserver
-from nti.dataserver.interfaces import IOIDResolver
-
-from nti.site.hostpolicy import get_all_host_sites
-
-
-@interface.implementer(IDataserver)
-class MockDataserver(object):
-
-    root = None
-
-    def get_by_oid(self, oid, ignore_creator=False):
-        resolver = component.queryUtility(IOIDResolver)
-        if resolver is None:
-            logger.warn("sing dataserver without a proper ISiteManager.")
-        else:
-            return resolver.get_object_by_oid(oid, ignore_creator=ignore_creator)
-        return None
 
 
 def index_site(current_site, catalog, intids,  seen):
@@ -62,38 +37,5 @@ def index_site(current_site, catalog, intids,  seen):
                 seen.add(package.ntiid)
 
 
-def do_evolve(context):
-    setHooks()
-    conn = context.connection
-    root = conn.root()
-    ds_folder = root['nti.dataserver']
-
-    mock_ds = MockDataserver()
-    mock_ds.root = ds_folder
-    component.provideUtility(mock_ds, IDataserver)
-
-    with site(ds_folder):
-        assert  component.getSiteManager() == ds_folder.getSiteManager(), \
-                "Hooks not installed?"
-
-        seen = set()
-        lsm = ds_folder.getSiteManager()
-        intids = lsm.getUtility(IIntIds)
-
-        library = component.queryUtility(IContentPackageLibrary)
-        if library is not None:
-            library.syncContentPackages()
-
-        catalog = install_library_catalog(ds_folder, intids)
-        for current_site in get_all_host_sites():
-            index_site(current_site, catalog, intids, seen)
-
-    component.getGlobalSiteManager().unregisterUtility(mock_ds, IDataserver)
-    logger.info('Dataserver evolution %s done. %s objects indexed', generation)
-
-
 def evolve(context):
-    """
-    Evolve to gen 3 by installing the library catalog.
-    """
-    do_evolve(context)
+    pass
