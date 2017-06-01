65d61368b16f6ff5ee374b2e93fc5302ed074467
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-06-01 01:21:16 +0000

    clean up
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.contentlibrary/trunk@113945 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/contentlibrary/views/asset_views.py
+++ src/nti/app/contentlibrary/views/asset_views.py
@@ -4,7 +4,7 @@
 .. $Id$
 """
 
-from __future__ import print_function, unicode_literals, absolute_import, division
+from __future__ import print_function, absolute_import, division
 __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
@@ -132,8 +132,9 @@ class ResetPackagePresentationAssetsView(_AbstractSyncAllLibrariesView):
         with current_site(get_host_site(site_name)):
             registry = component.getSiteManager()
             # remove using catalog
-            items.extend(clear_content_package_assets(package,
-                                                      force=force))
+            items.extend(
+                clear_content_package_assets(package, force=force)
+            )
             # remove anything left in containters
             for ntiid, item, container in self._unit_assets(package):
                 if can_be_removed(item, force=force):
