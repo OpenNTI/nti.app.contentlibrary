424840a2be7c81c18435187ac6542b8004b7e043
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-06-28 16:33:12 +0000

    WIP bundle views
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.contentlibrary/trunk@116122 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/contentlibrary/views/bundle_views.py
+++ src/nti/app/contentlibrary/views/bundle_views.py
@@ -9,13 +9,43 @@ __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
 
+import uuid
+
+from zope import component
+
+from zope import lifecycleevent
+
+from zope.cachedescriptors.property import Lazy
+
+from zope.component.hooks import site as current_site
+
+from pyramid import httpexceptions as hexc
+
 from pyramid.view import view_config
+
+from nti.app.base.abstract_views import AbstractAuthenticatedView
+
+from nti.app.contentlibrary import MessageFactory as _
+
+from nti.app.contentlibrary.views import LibraryPathAdapter
+
+from nti.app.externalization.error import raise_json_error
+
+from nti.app.externalization.view_mixins import ModeledContentUploadRequestUtilsMixin
+
+from nti.appserver.policies.interfaces import ISitePolicyUserEventListener
 
 from nti.appserver.ugd_edit_views import ContainerContextUGDPostView
 
 from nti.contentlibrary.interfaces import IContentPackageBundle
+from nti.contentlibrary.interfaces import IContentPackageBundleLibrary
+
+from nti.contentlibrary.utils import NTI
+from nti.contentlibrary.utils import make_content_package_bundle_ntiid 
 
 from nti.dataserver.authorization import ACT_READ
+
+from nti.site.interfaces import IHostPolicyFolder
 
 
 @view_config(route_name='objects.generic.traversal',
@@ -32,3 +62,71 @@ class ContentBundlePagesView(ContainerContextUGDPostView):
 
     Reading/Editing/Deleting will remain the same.
     """
+
+
+class ContentPackageBundleMixin(object):
+
+    @Lazy
+    def extra(self):
+        return str(uuid.uuid4()).split('-')[0].upper()
+
+    def get_library(self, context=None, provided=IContentPackageBundleLibrary):
+        if context is None:
+            library = component.queryUtility(provided)
+        else:
+            # If context is given, attempt to use the site the given context
+            # is stored in. This is necessary to avoid data loss during sync.
+            with current_site(IHostPolicyFolder(context)):
+                library = component.queryUtility(provided)
+        if library is None:
+            raise_json_error(self.request,
+                             hexc.HTTPUnprocessableEntity,
+                             {
+                                'message': _(u"Library not available."),
+                                'code': 'LibraryNotAvailable',
+                             },
+                             None)
+        return library
+
+    @classmethod
+    def make_bundle_ntiid(cls, provider=None, base=None, extra=None):
+        policy = component.queryUtility(ISitePolicyUserEventListener)
+        provider = provider or getattr(policy, 'PROVIDER', None) or NTI
+        return make_content_package_bundle_ntiid(provider, base, extra)
+
+
+@view_config(route_name='objects.generic.traversal',
+             renderer='rest',
+             request_method='POST',
+             context=LibraryPathAdapter,
+             permission=ACT_READ,
+             name='Bundles')
+class ContentBundlePostView(AbstractAuthenticatedView,
+                            ModeledContentUploadRequestUtilsMixin,
+                            ContentPackageBundleMixin):
+    
+    content_predicate = IContentPackageBundle
+
+    def readInput(self, value=None):
+        result = super(ContentBundlePostView, self).readInput(value)
+        result.pop('NTIID', None)
+        result.pop('ntiid', None)
+        return result
+
+    def _set_ntiid(self, context):
+        context.ntiid = self.make_bundle_ntiid(context)
+
+    def _do_call(self):
+        bundle_library = self.get_library(provided=IContentPackageBundleLibrary)
+        bundle = self.readCreateUpdateContentObject(self.remoteUser, 
+                                                    search_owner=False)
+        # set ntiid
+        self._set_ntiid(bundle)
+        # set creator
+        bundle.creator = self.remoteUser.username
+        # add to library
+        lifecycleevent.created(bundle)
+        bundle_library[bundle.ntiid] = bundle
+        self.request.response.status_int = 201
+        logger.info('Created new content package bundle (%s)', bundle.ntiid)
+        return bundle
