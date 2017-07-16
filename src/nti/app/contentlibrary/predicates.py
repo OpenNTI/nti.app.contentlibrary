e08ffba9b12b5ba8d579d486ff35f69e6207a9d9
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-07-16 19:28:19 +0000

    modify predicates to be site based
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.contentlibrary/trunk@117547 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/contentlibrary/predicates.py
+++ src/nti/app/contentlibrary/predicates.py
@@ -9,33 +9,19 @@ __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
 
-from functools import partial
-
 from zope import component
-
-from zope.cachedescriptors.property import Lazy
-
-from zope.intid.interfaces import IIntIds
 
 from nti.contentlibrary.interfaces import IContentPackageLibrary
 from nti.contentlibrary.interfaces import IEditableContentPackage
 from nti.contentlibrary.interfaces import IContentPackageBundleLibrary
-
-from nti.coremetadata.interfaces import SYSTEM_USER_ID
 
 from nti.dataserver.interfaces import IUser
 from nti.dataserver.interfaces import ISystemUserPrincipal
 
 from nti.dataserver.metadata.predicates import BasePrincipalObjects
 
-from nti.site.hostpolicy import run_job_in_all_host_sites
-
 
 class _PackageObjectsMixin(BasePrincipalObjects):
-
-    @Lazy
-    def intids(self):
-        return component.getUtility(IIntIds)
 
     @property
     def library(self):
@@ -44,9 +30,7 @@ class _PackageObjectsMixin(BasePrincipalObjects):
     def _content_units(self, context):
         result = []
         def _recur(obj):
-            doc_id = self.intids.queryId(obj)
-            if doc_id is not None:
-                result.append(obj)
+            result.append(obj)
             for child in obj.children or ():
                 _recur(child)
         _recur(context)
@@ -55,23 +39,12 @@ class _PackageObjectsMixin(BasePrincipalObjects):
     def _predicate(self, _):
         return False
 
-    def iter_items(self, result, seen):
-        library = self.library
-        if library is None:
-            return result
-        for item in library.contentPackages:
-            doc_id = self.intids.queryId(item)
-            if doc_id is not None and doc_id not in seen:
-                seen.add(doc_id)
-                if self._predicate(item):
-                    result.append(item)
-                    result.extend(self._content_units(item))
-        return result
-    
     def iter_objects(self):
         result = []
-        seen = set()
-        run_job_in_all_host_sites(partial(self.iter_items, result, seen))
+        if self.library is not None:
+            for item in self.library.contentPackages:
+                if self._predicate(item):
+                    result.extend(self._content_units(item))
         return result
 
 
@@ -79,54 +52,35 @@ class _PackageObjectsMixin(BasePrincipalObjects):
 class _SystemContentPackages(_PackageObjectsMixin):
 
     def _predicate(self, item):
-        return not IEditableContentPackage.providedBy(item)
+        return not IEditableContentPackage.providedBy(item) \
+            or self.is_system_username(self.creator(item))
 
 
 @component.adapter(IUser)
 class _UserContentPackages(_PackageObjectsMixin):
 
     def _predicate(self, item):
-        result = False
-        if IEditableContentPackage.providedBy(item):
-            creator = getattr(item.creator, 'username', None)
-            creator = getattr(creator, 'id', creator) or ''
-            result = creator.lower() == self.user.username.lower()
+        result = IEditableContentPackage.providedBy(item) \
+             and self.creator(item) == self.username
         return result
 
 
 class _BundleObjectsMixin(BasePrincipalObjects):
 
-    @Lazy
-    def intids(self):
-        return component.getUtility(IIntIds)
-
     @property
     def library(self):
         return component.queryUtility(IContentPackageBundleLibrary)
-
-    def get_creator(self, bundle):
-        creator = getattr(bundle.creator, 'username', None)
-        creator = getattr(creator, 'id', creator)
-        return creator or SYSTEM_USER_ID
         
     def _predicate(self, _):
         return False
 
-    def iter_items(self, result, seen):
+    def iter_objects(self):
+        result = []
         library = self.library
         bundles = library.getBundles() if library is not None else ()
         for item in bundles or ():
-            doc_id = self.intids.queryId(item)
-            if doc_id is not None and doc_id not in seen:
-                seen.add(doc_id)
-                if self._predicate(item):
-                    result.append(item)
-        return result
-
-    def iter_objects(self):
-        result = []
-        seen = set()
-        run_job_in_all_host_sites(partial(self.iter_items, result, seen))
+            if self._predicate(item):
+                result.append(item)
         return result
 
 
@@ -134,13 +88,11 @@ class _BundleObjectsMixin(BasePrincipalObjects):
 class _SystemContentPackageBundles(_BundleObjectsMixin):
 
     def _predicate(self, item):
-        creator = self.get_creator(item)
-        return creator == SYSTEM_USER_ID
+        return self.is_system_username(self.creator(item))
 
 
 @component.adapter(IUser)
 class _UserContentPackageBundles(_BundleObjectsMixin):
 
     def _predicate(self, item):
-        creator = self.get_creator(item)
-        return creator.lower() == self.user.username.lower()
+        return self.creator(item) == self.username
