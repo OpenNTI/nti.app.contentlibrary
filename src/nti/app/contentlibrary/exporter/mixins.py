718eed5456ab8319d0f0cdd95022ec4ce376ce16
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-08-15 21:31:35 +0000

    make sure we hash ntiids if needed
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.contentlibrary/trunk@119559 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/contentlibrary/exporter/mixins.py
+++ src/nti/app/contentlibrary/exporter/mixins.py
@@ -10,6 +10,7 @@ __docformat__ = "restructuredtext en"
 logger = __import__('logging').getLogger(__name__)
 
 from six import StringIO
+from collections import Mapping
 
 import simplejson
 
@@ -19,25 +20,47 @@ from nti.contenttypes.presentation.interfaces import IPresentationAssetContainer
 from nti.externalization.externalization import to_external_object
 
 from nti.externalization.interfaces import StandardExternalFields
+from nti.externalization.interfaces import StandardInternalFields
 
 from nti.mimetype.externalization import decorateMimeType
 
 from nti.ntiids.ntiids import hash_ntiid
 
+OID = StandardExternalFields.OID
 ITEMS = StandardExternalFields.ITEMS
+NTIID = StandardExternalFields.NTIID
 MIMETYPE = StandardExternalFields.MIMETYPE
+CREATED_TIME = StandardExternalFields.CREATED_TIME
+LAST_MODIFIED = StandardExternalFields.LAST_MODIFIED
+
+INTERNAL_NTIID = StandardInternalFields.NTIID
 
 
 class AssetExporterMixin(object):
 
     def __init__(self, *args, **kwargs):
-        AssetExporterMixin.__init__(self, *args, **kwargs)
+        super(AssetExporterMixin, self).__init__(*args, **kwargs)
 
     def dump(self, ext_obj):
         source = StringIO()
         simplejson.dump(ext_obj, source, indent='\t', sort_keys=True)
         source.seek(0)
         return source
+
+    def _prunner(self, ext_obj, backup=True, salt=None):
+        if isinstance(ext_obj, Mapping):
+            if not backup:
+                for name in (OID, CREATED_TIME, LAST_MODIFIED):
+                    ext_obj.pop(name, None)
+            else:
+                ext_obj.pop(OID, None)
+            for name, value in list(ext_obj.items()):
+                if not backup and value and name in (NTIID, INTERNAL_NTIID):
+                    ext_obj[name] = hash_ntiid(value, salt)
+                self._prunner(value, backup, salt)
+        if isinstance(ext_obj, (set, tuple, list)):
+            for value in ext_obj:
+                self._prunner(value, backup, salt)
 
     def do_export(self, package, provided=IPresentationAsset, backup=True, salt=None):
 
@@ -49,7 +72,7 @@ class AssetExporterMixin(object):
             if container:
                 ntiid = unit.ntiid
                 unit_ntiid = ntiid if backup else hash_ntiid(ntiid, salt)
-                containers.setdefault(unit.ntiid, [])
+                containers.setdefault(unit_ntiid, [])
                 for item in container.assets():
                     ntiid = item.ntiid
                     item_ntiid = ntiid if backup else hash_ntiid(ntiid, salt)
@@ -64,12 +87,16 @@ class AssetExporterMixin(object):
 
         items = dict()
         for asset in assets.values():
+            ntiid = asset.ntiid
+            asset_ntiid = ntiid if backup else hash_ntiid(ntiid, salt)
             ext_obj = to_external_object(asset,
                                          name="exporter",
                                          decorate=False)
+            self._prunner(ext_obj, backup, salt)
+            # hash ntiid
             decorateMimeType(asset, ext_obj)
             ext_obj['mimeType'] = ext_obj[MIMETYPE]
-            items[asset.ntiid] = ext_obj
+            items[asset_ntiid] = ext_obj
 
         result = dict()
         if items:
