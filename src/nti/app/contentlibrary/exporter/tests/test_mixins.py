915ca0e872a73b49240d3dfc1e6aed4818ee240b
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-08-15 22:50:23 +0000

    test merging
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.contentlibrary/trunk@119564 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/contentlibrary/exporter/tests/test_mixins.py
+++ src/nti/app/contentlibrary/exporter/tests/test_mixins.py
@@ -17,6 +17,9 @@ from hamcrest import assert_that
 from hamcrest import has_entries
 does_not = is_not
 
+import os
+import unittest
+
 from nti.app.contentlibrary.exporter.mixins import AssetExporterMixin
 
 from nti.contenttypes.presentation.interfaces import INTIVideo
@@ -33,23 +36,23 @@ from nti.app.testing.decorators import WithSharedApplicationMockDS
 from nti.dataserver.tests import mock_dataserver
 
 
-class TestMixins(ApplicationLayerTest):
-
+class TestExport(ApplicationLayerTest):
+ 
     layer = PersistentApplicationTestLayer
-
+ 
     default_origin = 'http://janux.ou.edu'
-
+ 
     package = u"tag:nextthought.com,2011-10:OU-HTML-CS1323_F_2015_Intro_to_Computer_Programming.introduction_to_computer_programming"
-
+ 
     video = u'tag:nextthought.com,2011-10:OU-NTIVideo-CS1323_F_2015_Intro_to_Computer_Programming.ntivideo.video_janux_videos'
-
+ 
     container = u'tag:nextthought.com,2011-10:OU-HTML-CS1323_F_2015_Intro_to_Computer_Programming.lec:01.04_LESSON'
-
+ 
     salt = '100'
-
+ 
     @WithSharedApplicationMockDS(testapp=False, users=False)
     def test_export(self):
-
+ 
         with mock_dataserver.mock_db_trans(self.ds, site_name='platform.ou.edu'):
             package = find_object_with_ntiid(self.package)
             exporter = AssetExporterMixin()
@@ -57,16 +60,29 @@ class TestMixins(ApplicationLayerTest):
             assert_that(result, 
                         has_entries('Containers', has_length(32),
                                     'Items', has_length(92)))
-            
+             
             salted_video = hash_ntiid(self.video, self.salt)
             assert_that(result,
                         has_entry('Items', has_entry(salted_video, 
                                                      has_entries('NTIID', salted_video,
                                                                  'MimeType', is_not(none())))))
-            
+             
             salted_container = hash_ntiid(self.container, self.salt)
             assert_that(result,
                         has_entry('Containers', has_key(salted_container)))
-            
+             
             items = result['Containers'][salted_container]
             assert_that(salted_video, is_in(items))
+
+
+class TestMerge(unittest.TestCase):
+
+    def test_merge(self):
+        result = dict()
+        source = os.path.join(os.path.dirname(__file__), 'video_index.json')
+        for _ in range(2):
+            with open(source, "r") as fp:
+                AssetExporterMixin.merge(result, fp)
+                assert_that(result, 
+                            has_entries('Containers', has_length(44),
+                                        'Items', has_length(94)))
