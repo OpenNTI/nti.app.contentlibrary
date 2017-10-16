5ccc344927afd338979820ffbffda863ed63dc5c
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-10-16 03:34:07 +0000

    add notify flag to script
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.contentlibrary/trunk@123166 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/contentlibrary/scripts/nti_sync_all_libraries.py
+++ src/nti/app/contentlibrary/scripts/nti_sync_all_libraries.py
@@ -27,10 +27,11 @@ from nti.dataserver.utils.base_script import create_context
 logger = __import__('logging').getLogger(__name__)
 
 
-def _process_args(site, ntiids, removal):
+def _process_args(site, ntiids, removal=True, notify=True):
     library = component.getUtility(IContentPackageLibrary)
     library.syncContentPackages()
-    return synchronize(site=site, ntiids=ntiids, allowRemoval=removal)
+    return synchronize(site=site, ntiids=ntiids,
+                       allowRemoval=removal, notify=notify)
 
 
 def main():
@@ -40,7 +41,9 @@ def main():
     arg_parser.add_argument('-s', '--site', help="Sites name",
                             dest='site', default=None)
     arg_parser.add_argument('-r', '--removal', help="Allow removal", action='store_true',
-                            dest='removal')
+                            dest='removal', default=False)
+    arg_parser.add_argument('-t', '--notify', help="Notify completion", action='store_true',
+                            dest='notify', default=False)
     arg_parser.add_argument('-n', '--ntiids', help="NTIIDs", nargs="+",
                             dest='ntiids', default=())
 
@@ -51,6 +54,7 @@ def main():
         raise IOError("Invalid dataserver environment root directory")
 
     site = args.site
+    notify = args.notify
     removal = args.removal
     ntiids = set(args.ntiids) if args.ntiids else ()
     ntiids = map(text_, ntiids)
@@ -63,9 +67,7 @@ def main():
                         xmlconfig_packages=conf_packages,
                         verbose=args.verbose,
                         context=context,
-                        function=lambda: _process_args(site=site,
-                                                       ntiids=ntiids,
-                                                       removal=removal))
+                        function=lambda: _process_args(site, ntiids, removal, notify))
 
     sys.exit(0)
 
