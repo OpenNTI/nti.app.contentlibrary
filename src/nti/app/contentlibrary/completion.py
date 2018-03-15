8054cb205a0e4457827366cff6f8927b78db8307
Josh Zuech <josh.zuech@nextthought.com>
2018-03-15 15:29:59 +0000

    Add default completion policy for content units
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.contentlibrary/trunk@127173 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ src/nti/app/contentlibrary/completion.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Adapters for application-level events.
+
+.. $Id$
+"""
+
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
+
+from zope import component
+from zope import interface
+
+from nti.contentlibrary.interfaces import IContentUnit
+
+from nti.contenttypes.completion.interfaces import ICompletionContext
+from nti.contenttypes.completion.interfaces import ICompletableItemCompletionPolicy
+from nti.contenttypes.completion.interfaces import ICompletionContextCompletionPolicyContainer
+
+logger = __import__('logging').getLogger(__name__)
+
+
+@component.adapter(IContentUnit)
+@interface.implementer(ICompletableItemCompletionPolicy)
+class DefaultContentUnitCompletionPolicy(object):
+    """
+    A simple completion policy that only cares about submissions for completion.
+    """
+
+    def __init__(self, obj):
+        self.content_unit = obj
+
+    def is_complete(self, progress):
+        return progress is not None and progress.HasProgress
+
+
+@component.adapter(IContentUnit, ICompletionContext)
+@interface.implementer(ICompletableItemCompletionPolicy)
+def _content_completion_policy(content_unit, course):
+    """
+    Fetch the :class:`ICompletableItemCompletionPolicy` for this
+    :class:`IContentUnit` and :class:`ICompletionContext`.
+    """
+    # First see if we have a specific policy set on our context.
+    context_policies = ICompletionContextCompletionPolicyContainer(course)
+    try:
+        result = context_policies[content_unit.ntiid]
+    except KeyError:
+        # Ok, fetch the default
+        result = ICompletableItemCompletionPolicy(content_unit)
+    return result
+
