f5732142e875b9e5062250a258f2c0cf5786be46
Carlos Sanchez <carlos.sanchez@nextthought.com>
2018-08-27 21:24:49 +0000

    do some current request filtering for content bundles. We really need a view, but let's do it here for now
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.contentlibrary/trunk@130710 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/contentlibrary/workspaces/externalization.py
+++ src/nti/app/contentlibrary/workspaces/externalization.py
@@ -4,13 +4,20 @@
 .. $Id$
 """
 
-from __future__ import print_function, absolute_import, division
-__docformat__ = "restructuredtext en"
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
 
-logger = __import__('logging').getLogger(__name__)
+from pyramid.threadlocal import get_current_request
+
+from requests.structures import CaseInsensitiveDict
+
+from six.moves.urllib_parse import unquote
 
 from zope import component
 from zope import interface
+
+from zope.cachedescriptors.property import Lazy
 
 from nti.app.contentlibrary.workspaces.interfaces import ILibraryCollection
 
@@ -18,6 +25,8 @@ from nti.externalization.externalization import to_external_object
 
 from nti.externalization.interfaces import IExternalObject
 from nti.externalization.interfaces import LocatedExternalDict
+
+logger = __import__('logging').getLogger(__name__)
 
 
 @component.adapter(ILibraryCollection)
@@ -27,18 +36,49 @@ class LibraryCollectionDetailExternalizer(object):
     Externalizes a Library wrapped as a collection.
     """
 
-    # TODO: This doesn't do a good job of externalizing it,
+    # This doesn't do a good job of externalizing it,
     # though. We're skipping all the actual Collection parts
 
     def __init__(self, collection):
         self._collection = collection
 
+    @property
+    def request(self):
+        return get_current_request()
+
+    @Lazy
+    def params(self):
+        params = self.request.params if self.request else {}
+        return CaseInsensitiveDict(**params)
+
+    @Lazy
+    def searchTerm(self):
+        # pylint: disable=no-member
+        result = self.params.get('searchTerm') or self.params.get('filter')
+        return unquote(result).lower() if result else None
+
+    def search_prefix_match(self, compare, search_term):
+        compare = compare.lower() if compare else ''
+        for k in compare.split():
+            if k.startswith(search_term):
+                return True
+        return compare.startswith(search_term)
+
+    def search_include(self, bundle):
+        result = True
+        if self.searchTerm:
+            op = self.search_prefix_match
+            result = op(bundle.title, self.searchTerm)
+        return result
+    
     def toExternalObject(self, **kwargs):
-        library_items = self._collection.library_items
+        items = self._collection.library_items
         result = LocatedExternalDict(
             {
                 'title': "Library",
-                'titles': [to_external_object(x, **kwargs) for x in library_items]
+                'titles': [
+                    to_external_object(x, **kwargs) for x in items if self.search_include(x)
+                ]
             })
         result.__name__ = self._collection.__name__
         result.__parent__ = self._collection.__parent__
