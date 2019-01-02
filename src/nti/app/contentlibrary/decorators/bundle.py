91770932d5d0e5310678b697d8473492963392af
Josh Zuech <josh.zuech@nextthought.com>
2019-01-02 17:06:04 +0000

    Do not need special bundle publish link handling
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.contentlibrary/trunk@131220 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/contentlibrary/decorators/bundle.py
+++ src/nti/app/contentlibrary/decorators/bundle.py
@@ -116,36 +116,6 @@ class _ContentBundleDecorator(AbstractAuthenticatedRequestAwareDecorator):
             _links.append(link)
 
 
-@interface.implementer(IExternalMappingDecorator)
-@component.adapter(IPublishableContentPackageBundle, IRequest)
-class _PublishableContentPackageBundleDecorator(AbstractAuthenticatedRequestAwareDecorator):
-
-    def _predicate(self, context, unused_result):
-        result = self._is_authenticated \
-             and has_permission(ACT_CONTENT_EDIT, context, self.request)
-        return result
-
-    def _need_publish_link(self, context):
-        return context.lastModified \
-           and context.publishLastModified \
-           and context.lastModified > context.publishLastModified
-
-    def _do_decorate_external(self, context, result):
-        rels = ()
-        if context.is_published():
-            rels = (VIEW_UNPUBLISH,)
-        elif self._need_publish_link(context):
-            rels = (VIEW_PUBLISH,)
-        _links = result.setdefault(LINKS, [])
-        for rel in rels:
-            link = Link(context,
-                        rel=rel,
-                        elements=('@@%s' % rel,))
-            link.__name__ = ''
-            link.__parent__ = context
-            _links.append(link)
-
-
 @component.adapter(IUser)
 @interface.implementer(IExternalMappingDecorator)
 class _UserBundleRecordsDecorator(AbstractAuthenticatedRequestAwareDecorator):
